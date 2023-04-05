d1153bdebbe4d028de016c6b3dd76158fc69b774
tomherden@gmail.com <tomherden@gmail.com>
2023-04-05 18:57:21 +0000

    Edited About, Main, User, settings and 1 more

--- client_code/About/__init__.py
+++ client_code/About/__init__.py
@@ -10,8 +10,5 @@ class About(AboutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    # text = app_tables.files.get(name='about.md')['data'].get_bytes()
-    # self.rich_text_1.content = text.decode('utf-8')
-    self.rich_text_1 = get_files_table_data('about.md', 'data')
-    self.rich_text_1 = anvil.server.call('get_files_table_data', 'about.md', 'data')
+    self.rich_text_1.content = anvil.server.call('get_files_table_data', 'about.md', 'data')
 
--- client_code/User/__init__.py
+++ client_code/User/__init__.py
@@ -13,3 +13,13 @@ class User(UserTemplate):
     self.carrier_dd.items = [(r['carrier'], r) for r in app_tables.carriers.search(tables.order_by('carrier'))]
     self.location_dd.items = [(r['name'], r) for r in app_tables.locations.search(tables.order_by('name'))]
     # Any code you write here will run before the form opens.
+
+  def notify_by_text_checkbox_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    text_components = [self.carrier_dd, self.cellphone_box, self.label_2, self.label_3]
+    if self.notify_by_text_checkbox.checked:
+      for each in text_components:
+        each.visible = True
+    else:
+      for each in text_components:
+        each.visible = False  
