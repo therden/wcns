0cc04128add314b08e5cc877524ac5fb13bba6ea
tomherden@gmail.com <tomherden@gmail.com>
2024-08-08 20:20:23 +0000

    Created LICENSE.txt; Edited User

--- client_code/User/__init__.py
+++ client_code/User/__init__.py
@@ -14,6 +14,7 @@ class User(UserTemplate):
     self.carrier_dd.items = [(r['carrier'], r) for r in app_tables.carriers.search(tables.order_by('carrier'))]
     self.user_copy = dict(anvil.server.call('get_user')) if anvil.server.call('get_user') else dict()
     self.item = self.user_copy
+    self.headline.text = "View/Edit User: " + self.item['email']
     self.toggle_text_components()
 
 
