34e9856becd4e4bd7fdb5171b62a672f439c20a4
tomherden@gmail.com <tomherden@gmail.com>
2024-08-15 22:18:28 +0000

    Edited User and theme/parameters.yaml

--- client_code/User/__init__.py
+++ client_code/User/__init__.py
@@ -12,9 +12,13 @@ class User(UserTemplate):
     self.init_components(**properties)
     self.location_dd.items = [(r['name'], r) for r in app_tables.locations.search(tables.order_by('name'))]
     self.carrier_dd.items = [(r['carrier'], r) for r in app_tables.carriers.search(tables.order_by('carrier'))]
-    self.user_copy = dict(anvil.server.call('get_user')) if anvil.server.call('get_user') else dict()
-    self.item = self.user_copy
-    self.headline.text = "View/Edit User: " + self.item['email']
+    user = anvil.server.call('get_user')
+    if user:
+      self.user_copy = dict(user)
+      self.item = self.user_copy
+      self.headline.text = "View/Edit User: " + self.item['email']
+    else:
+      self.user_copy = dict()
     self.toggle_text_components()
 
 
